0fa462c72e9fc54be8bed00759685a46bfc52551
AMikail0402 <alpay.yildiz@outlook.com>
2024-03-10 19:50:20 +0100

    changes_in_startup

--- startup.sh
+++ startup.sh
@@ -3,7 +3,5 @@ docker kill account-api
 docker rm account-api
 docker kill account-db
 docker rm account-db
-docker-compose -f db-config.yml up -d
 mvn clean install spring-boot:repackage
-docker build -t account . 
-docker run -d --name account-api -p 8080:8080 account
+docker-compose -f db-config.yml up -d
