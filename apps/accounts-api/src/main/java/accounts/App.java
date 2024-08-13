944522fcb524959423ed4606d79767dcad56ac78
AMikail0402 <alpay.yildiz@outlook.com>
2024-08-13 16:28:27 +0200

    grpc

---
+++ apps/accounts-api/src/main/java/accounts/App.java
@@ -0,0 +1,31 @@
+package accounts;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.ServerCredentials;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) throws Exception{
+
+        new SpringApplicationBuilder(App.class)
+        .web(WebApplicationType.NONE)
+        .run();
+
+        ServerCredentials credential = InsecureServerCredentials.create();
+        Server server = Grpc.newServerBuilderForPort(8081, credential).build().start();
+        server.awaitTermination();
+    }
+
+}
