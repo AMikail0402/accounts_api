20652e9cd54aa01b9fd7489bf5dc0b1e77dc258f
AMikail0402 <alpay.yildiz@outlook.com>
2024-12-13 12:23:22 +0100

    db password

---
+++ replicationcommands.sql
@@ -0,0 +1,30 @@
+-- Publication of db 1
+CREATE PUBLICATION pub_pri1 FOR ALL TABLES;
+
+
+-- Subscription for DB 2
+CREATE SUBSCRIPTION sub_pri2_pri1
+CONNECTION 'dbname=datenbank host=accounts-db-postgresql.db user=postgres password=wHcYhamkn3'
+PUBLICATION pub_pri1 WITH (origin = NONE, copy_data = false);
+
+-- set wal level
+ALTER SYSTEM SET wal_level = logical;
+
+
+-- publication for DB 2 
+CREATE PUBLICATION pub_pri2 FOR ALL TABLES;
+
+-- Subscription for DB 1
+CREATE SUBSCRIPTION sub_pri1_pri2
+CONNECTION 'dbname=datenbank host=accounts-db-2-postgresql.db-2 user=postgres password=NWO4Dc5kQ3'
+PUBLICATION pub_pri2 WITH (origin = NONE, copy_data = false);
+
+-- synchronous commits for node 1 
+ALTER SYSTEM SET synchronous_commit = 'on';
+ALTER SYSTEM SET synchronous_standby_names = 'sub_pri2_pri1';
+SELECT pg_reload_conf();
+
+-- synchronous commits for node 2
+ALTER SYSTEM SET synchronous_commit = 'on';
+ALTER SYSTEM SET synchronous_standby_names = 'sub_pri1_pri2';
+SELECT pg_reload_conf();
