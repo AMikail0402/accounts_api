d1b228a7d8c35093642302abc077763fdde71096
AMikail0402 <alpay.yildiz@outlook.com>
2024-12-13 13:23:26 +0100

    Proof of Concept

---
+++ deployment/loadtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in {1..200}; do
+  curl --request POST \
+  --url http://localhost/api/user \
+  --header 'Content-Type: application/json' \
+  --header 'User-Agent: insomnia/2023.5.8' \
+  --data '{
+	"family_name": "testFamily",
+	"surname": "test test", 
+	"address": "test_address",
+	"phone_number": "014122233"
+  }'
+done
