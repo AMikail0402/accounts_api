865e7a0a6a989c557e4e2972e718084c821193b2
AMikail0402 <alpay.yildiz@outlook.com>
2025-01-17 20:18:35 +0100

    helm

--- deployment/db/role.sql
+++ deployment/db/role.sql
@@ -2,6 +2,7 @@ GRANT  ALL PRIVILEGES   ON SCHEMA public  TO transfer;
 ALTER ROLE transfer LOGIN;
 GRANT REFERENCES ON ALL TABLES IN SCHEMA public TO transfer;
 
+-- TODO: Select on table
 
 -- sources: https://stackoverflow.com/questions/17338621/what-does-grant-usage-on-schema-do-exactly
 --          https://stackoverflow.com/questions/35254786/postgresql-role-is-not-permitted-to-log-in
